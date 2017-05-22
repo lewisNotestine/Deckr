808c21be24dba05913c6ed7e98f2f14a7ce8584f
Lewis S Notestine <lewis.notestine@gmail.com>
2017-05-21 23:33:33 -0700

    for console app, make suit symbols print ok
    Using UTF 8 encoding so it works for bash and windows cmd prompt

--- Deckr.Console/Program.cs
+++ Deckr.Console/Program.cs
@@ -7,6 +7,9 @@ namespace Deckr.Console
     {
         static void Main(string[] args)
         {
+            //Setting the encoding makes printing work on Windows & Non-windows envts.
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             var cmdLine = new CmdLine(Deckr.GetDefault(), new ConsoleWrapper());
             CmdLineAction currentAction;
             do
